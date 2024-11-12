26b181241e1d09d926ff2685d749978403da8888
Harish0705 <harish.balachandar@nzme.co.nz>
2024-11-12 10:52:41 +0530

    input validation using validation pipe and class validator

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  /* By setting whitelist to true , any additional fields/properties that are passed to the request from client will be stripped automatically by NestJS. ValidationPipe will automatically remove all non-whitelisted properties, where “non-whitelisted” means properties without any validation decorators. It’s important to note that this option will filter all properties without validation decorators, even if they are defined in the DTO. */
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   const config = new DocumentBuilder()
     .setTitle('Nest Js Blog App')
     .setDescription('Nest Js Blog API description')
